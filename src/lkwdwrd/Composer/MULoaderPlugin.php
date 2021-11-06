2e5d823c9348ba6c4b9afad456d2953d6df3f44a
Anders Magnusson <anders.magnusson@angrycreative.se>
2021-11-06 14:17:30 +0100

    composer 2 compability

--- src/lkwdwrd/Composer/MULoaderPlugin.php
+++ src/lkwdwrd/Composer/MULoaderPlugin.php
@@ -212,4 +212,12 @@ class MULoaderPlugin implements PluginInterface, EventSubscriberInterface {
 
 		return $separator;
 	}
+
+	public function deactivate( \Composer\Composer $composer, \Composer\IO\IOInterface $io) {
+
+	}
+
+	public function uninstall( \Composer\Composer $composer, \Composer\IO\IOInterface $io ) {
+
+	}
 }
